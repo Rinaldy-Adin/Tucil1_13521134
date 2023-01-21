75aec14e4ce0d8d3e63bb061f432aaa69b4456c3
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-01-21 12:34:17 +0700

    fix: reading input; input validation;

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@ using std::cout;
 using std::endl;
 using std::vector;
 
+/*
+    CHECK FOR: 10 3 9 6
+*/
+
 int main() {
     int cardNums[4];
     int permutations[25][4];
@@ -33,22 +37,28 @@ int main() {
 
     readInput(cardNums);
 
-    auto begin = std::chrono::high_resolution_clock::now();
+    if (cardNums[0] != -1) {
+        auto begin = std::chrono::high_resolution_clock::now();
 
-    generatePermutations(cardNums, permutations, &nPermutations);
-    generateExpressions(possibleExpressions, &nExpressions);
-    vector<string> expressions =
-        evaluateExpressions(permutations, nPermutations, possibleExpressions, nExpressions);
+        generatePermutations(cardNums, permutations, &nPermutations);
+        generateExpressions(possibleExpressions, &nExpressions);
+        vector<string> expressions =
+            evaluateExpressions(permutations, nPermutations, possibleExpressions, nExpressions);
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
 
-    cout << expressions.size() << " solutions found:" << endl << endl;
-    for (string expression : expressions) {
-        cout << expression << endl;
+        cout << expressions.size() << " solutions found:" << endl << endl;
+        for (string expression : expressions) {
+            cout << expression << endl;
+        }
+
+        printf("\nAlgorithms elapsed for %.3f miliseconds\n", elapsed.count() * 1e-6);
+    } else {
+        printf(
+            "\nInput invalid, enter 4 card values separated by spaces\n(A, 2, 3, 4, 5, 6, 7, 8, 9, "
+            "10, J, Q, K)\n");
     }
-
-    printf("\nAlgorithms elapsed for %.3f miliseconds\n", elapsed.count() * 1e-6);
 
     return 0;
 }
